ad4096a073ee7a52da16a34c218eb38a0f3c0d39
Joachim666 <joachimotto@wp.pl>
2017-03-01 19:42:50 +0100

    change

--- app/src/main/java/com/example/http/simpleintentservice/SimpleIntentService.java
+++ app/src/main/java/com/example/http/simpleintentservice/SimpleIntentService.java
@@ -3,6 +3,7 @@ package com.example.http.simpleintentservice;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 
 public class SimpleIntentService extends IntentService {
@@ -15,17 +16,27 @@ public class SimpleIntentService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
             String action = intent.getAction();
+            double valueA = intent.getDoubleExtra("extra.VALUE_A",0);
+            double valueB = intent.getDoubleExtra("extra.VALUE_B",0); //8
+            Log.i("TEST","Nazwa akcji " + action); //7
 
-            if ("NAZWA_AKCJI_1".equals(action)) {
+            if ("action.PLUS".equals(action)) {
+                double result = add(valueA,valueB);//9
+                Log.i("TEST", "wyniki: " + result); //10
+                broadcastResult( result);//12
+
+
+
                 //TODO pobierz parametry, oblicz i odeślij wynik
             }
             else if ("NAZWA_AKCJI_2".equals(action)) {
+
                 //TODO pobierz parametry, oblicz i odeślij wynik
             }
         }
     }
 
-    private void broadcastResult(double result) {
+    private void broadcastResult(double result) {//11
         Intent intent = new Intent();
         intent.setAction("action.CALCULATION_RESULT");
         intent.putExtra("extra.VALUE", result);
